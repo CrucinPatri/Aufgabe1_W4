4b3b65b744344139b28d80afb3dfd165305b3939
Aministrator <crucin.patricia@yahoo.com>
2025-10-20 20:03:08 +0300

    Aufgabe1 - 2. Medie

---
+++ src/Durchschnittswert.java
@@ -0,0 +1,12 @@
+public class Durchschnittswert {
+    public static double getDurchsnitt(int[] grades) {
+        int sum=0;
+
+        for(int grade : grades)
+            sum+=grade;
+
+        double medie = (double) sum / grades.length;
+
+        return Math.round(medie*100)/100.0;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args) {
         int grades[] = {29, 37, 38, 41, 84, 67};
 
-        //nicht ausreichend
+        // nicht ausreichend
         int[] insufficient = NichtAusreichendeNote.InsufficientNotes(grades);
         System.out.println("Nicht ausreichende Noten: " + Arrays.toString(insufficient));
 
+        // Mittelnote.
+        double medie = Durchschnittswert.getDurchsnitt(grades);
+        System.out.println("Durchschnittswert: " + medie);
     }
 }
